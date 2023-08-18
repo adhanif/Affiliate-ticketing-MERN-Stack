52ae38374b645a99be1b2f3e5b300397e5dc12ea
ad.hanif <ad.hanif@outlook.com>
2023-08-18 14:53:12 +0200

    created backend express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const port = 3005;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("It is working");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`);
+});
